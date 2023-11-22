f1926d97a969ec36357a83dc39b1d731915018c2
Mjdfsh <mjdfalshehri@gmail.com>
2023-11-23 01:26:21 +0300

    added break statement in the nested loop by Majd

--- SensorDataProcessor.java
+++ SensorDataProcessor.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class SensorDataProcessor {
 // Senson data and limits.
 public double[][][] data;
@@ -25,31 +28,38 @@ BufferedWriter out;
 // Write racing stats data into a file
 try {
 out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
+
 for (i = 0; i < data.length; i++) {
-for (j = 0; j < data[0].length; j++) {
-for (k = 0; k < data[0][0].length; k++) {
-data2[i][j][k] = data[i][j][k] / d -
-Math.pow(limit[i][j], 2.0);
-if (average(data2[i][j]) > 10 && average(data2[i][j]) 
-< 50)
-break;
-else if (Math.max(data[i][j][k], data2[i][j][k]) > 
-data[i][j][k])
-break;
-else if (Math.pow(Math.abs(data[i][j][k]), 3) < 
-Math.pow(Math.abs(data2[i][j][k]), 3)
-&& average(data[i][j]) < data2[i][j][k] && (i + 1) 
-* (j + 1) > 0)
-data2[i][j][k] *= 2;
-else
-continue;
+    for (j = 0; j < data[0].length; j++) {
+        for (k = 0; k < data[0][0].length; k++) {
+           data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
+
+            if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
+                break;
+            else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
+                break;
+            else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
+                && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0)
+                data2[i][j][k] *= 2;
+            else
+                continue;
+        }
+        if (k < data[0][0].length)
+            break;
+    }
+    if (j < data[0].length)
+        break;
 }
-}
-}
+
+ 
+
 for (i = 0; i < data2.length; i++) {
-for (j = 0; j < data2[0].length; j++) {
-out.write(data2[i][j] + "\t");
-}
+    for (j = 0; j < data2[0].length; j++) {
+        for (k = 0; k < data2[0][0].length; k++) {
+            out.write(data2[i][j][k] + "\t");
+        }
+    }
+    out.newLine();
 }
 out.close();
 } catch (Exception e) {
